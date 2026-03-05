a2fe7fbd988abb7c9f32bec58fbe23bf521548f8
Julio Rocha <julioeliasdesousarocha.work@gmail.com>
2026-03-05 11:15:48 -0300

    Service layer, component registration

--- src/main/java/com/julio/SBProject/repositories/UserRepository.java
+++ src/main/java/com/julio/SBProject/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.julio.SBProject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.julio.SBProject.entites.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	// Faz o CRUD para a classe User automaticamente.
 }
--- src/main/java/com/julio/SBProject/resources/UserResource.java
+++ src/main/java/com/julio/SBProject/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.julio.SBProject.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.julio.SBProject.entites.User;
+import com.julio.SBProject.services.UserService;
+
 
 @RestController
 @RequestMapping (value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Julio", "julio@gmail.com", "99 9 9999-9999", "123456");
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<List<User>> findAll() {
+		return ResponseEntity.ok().body(service.findAll());
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> FindById(@PathVariable Long id) {
+		return ResponseEntity.ok().body(service.findById(id));
+	}
+	
 }
